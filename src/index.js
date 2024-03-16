82c17113aa4f0a2425b4ebb7a55542bb9e3152ba
Ana Pedra <106017493+redjules@users.noreply.github.com>
2024-03-16 17:14:29 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ class ChatGPTExtension {
     }
 
     prompt1 = "From now on, if you need to write a mathematical expression, use katex notation and follow these rules:\n1. If it is a block equation, display it in a single P element and wrap it with double dollar signs like this:\n\n$$e=mc^{2}$$\n\n2. If it is an inline equation, use the two backlash and parenthesis notation of katex, like this: \\(e^{i \\\pi}-1=0\\).\n\nCan you give me an example of a bloack equation to see that you understand?"
-    prompt2 = "Say something, but in French"
+    prompt2 = "Say something smart, but in French"
 
 
 const CGPTExtension = new ChatGPTExtension()
